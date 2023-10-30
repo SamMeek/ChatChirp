df2e8ec30c91e1227c5795cd201efef71f2c440a
Samapan Mondal <89137397+SamMeek@users.noreply.github.com>
2023-10-30 21:23:52 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,11 +9,11 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Realtime Chat App with Ably, NextJS and Vercel</title>
+        <title>Chat Chirp</title>
         <link rel="icon" href="https://static.ably.dev/motif-red.svg?nextjs-vercel" type="image/svg+xml" />
       </Head>
       <main>
-        <h1 className="title">Next.js Chat Demo</h1>
+        <h1 className="title">Chat Chirp</h1>
         <Chat />
       </main>
       <footer>
